54c73a49380547b4a468d553fb2142d7f2804901
nuriho <31554893+nuriho@users.noreply.github.com>
2017-12-16 17:48:44 -0700

    Create ClassUtils.java

---
+++ utils/ClassUtils.java
@@ -0,0 +1,110 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2017 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package utils;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Useful methods for working with {@link Class} objects and primitive types.
+ *
+ * @author Curtis Rueden
+ */
+public final class ClassUtils {
+
+	private ClassUtils() {
+		// prevent instantiation of utility class
+	}
+	// -- Class loading, querying and reflection --
+
+	/**
+	 * Gets the base location of the given class.
+	 * <p>
+	 * If the class is directly on the file system (e.g.,
+	 * "/path/to/my/package/MyClass.class") then it will return the base directory
+	 * (e.g., "file:/path/to").
+	 * </p>
+	 * <p>
+	 * If the class is within a JAR file (e.g.,
+	 * "/path/to/my-jar.jar!/my/package/MyClass.class") then it will return the
+	 * path to the JAR (e.g., "file:/path/to/my-jar.jar").
+	 * </p>
+	 *
+	 * @param c The class whose location is desired.
+	 * @see FileUtils#urlToFile(URL) to convert the result to a {@link File}.
+	 */
+	public static URL getLocation(final Class<?> c) {
+		if (c == null) return null; // could not load the class
+
+		// try the easy way first
+		try {
+			final URL codeSourceLocation =
+				c.getProtectionDomain().getCodeSource().getLocation();
+			if (codeSourceLocation != null) return codeSourceLocation;
+		}
+		catch (final SecurityException e) {
+			// NB: Cannot access protection domain.
+		}
+		catch (final NullPointerException e) {
+			// NB: Protection domain or code source is null.
+		}
+
+		// NB: The easy way failed, so we try the hard way. We ask for the class
+		// itself as a resource, then strip the class's path from the URL string,
+		// leaving the base path.
+
+		// get the class's raw resource path
+		final URL classResource = c.getResource(c.getSimpleName() + ".class");
+		if (classResource == null) return null; // cannot find class resource
+
+		final String url = classResource.toString();
+		final String suffix = c.getCanonicalName().replace('.', '/') + ".class";
+		if (!url.endsWith(suffix)) return null; // weird URL
+
+		// strip the class's path from the URL string
+		final String base = url.substring(0, url.length() - suffix.length());
+
+		String path = base;
+
+		// remove the "jar:" prefix and "!/" suffix, if present
+		if (path.startsWith("jar:")) path = path.substring(4, path.length() - 2);
+
+		try {
+			return new URL(path);
+		}
+		catch (final MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
