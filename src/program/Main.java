8fb8b54332074ab2e6d095a9654778a34b6e502c
nuriho <31554893+nuriho@users.noreply.github.com>
2017-12-16 17:53:02 -0700

    Update Main.java

--- src/program/Main.java
+++ src/program/Main.java
@@ -7,30 +7,50 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 
-public class Main {
-	
-	public static void main(String[] args) {
+import utils.ClassUtils;
+import utils.FileUtils;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
 		DssNameChange dnc = new DssNameChange();
 		// String path = "E:/eh/기황후 (Ki Empress)";
 		String path = "";
-		File file = new File("E:/workspace/dssNameChange/path.txt");
+		StringBuilder filePath = new StringBuilder();
+
+		URL classUrl = ClassUtils.getLocation(Main.class);
+		File jarFile = FileUtils.urlToFile(classUrl);
+		// should be ~/dssNameChange/bin
+		String jarDir = jarFile.getParentFile().getPath();
+		// should be ~/dssNameChange
+		filePath.append(jarDir);
+		filePath.append(File.separator);
+		filePath.append("path.txt");
+		File file = new File(filePath.toString());
 		BufferedReader br;
-		try {
+		try
+		{
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
 			String line = br.readLine();
-			
-			if (line != null) {
+
+			if (line != null)
+			{
 				path = line.substring(1, line.length());
 			}
 			dnc.main(path);
-		} catch (UnsupportedEncodingException | FileNotFoundException e) {
+		} catch (UnsupportedEncodingException | FileNotFoundException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 }
